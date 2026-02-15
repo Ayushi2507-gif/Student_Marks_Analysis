e8d4dd61a58db554d66fe3d0e5a38ed4c4264bc2
Ayushi2507-gif <gupta.ayushi99888@gmail.com>
2026-02-15 15:47:27 +0530

    Add files via upload

--- MAIN.py
+++ MAIN.py
@@ -2,15 +2,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Read CSV file
+# Read CSV file 
 data = pd.read_csv("AYU.csv")
 print(data)
 
 # Create bar chart
 plt.figure(figsize=(8,5))   
 plt.bar(data['Name'],data['Marks'])
-
-# Add title and labels
 plt.title("Student Marks Analysis")
 plt.xlabel("Students")
 plt.ylabel("Marks")
